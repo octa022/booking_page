28a3b9b371ac20e8bf4c7d1aee108cdbb63c70c2
Octavio Torres <octa022@gmail.com>
2020-05-20 01:25:27 -0400

    Updates

---
+++ precache-manifest.936a7a8a57667ce74780f6fcd9ba3a8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb37deb89c57ea432d369b75fdc6e958",
+    "url": "/booking_page/index.html"
+  },
+  {
+    "revision": "c6ac763d8a987679f3c2",
+    "url": "/booking_page/static/css/main.9f6c6838.chunk.css"
+  },
+  {
+    "revision": "8f0bec4d86bf445ffcda",
+    "url": "/booking_page/static/js/2.e0ecbee2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/booking_page/static/js/2.e0ecbee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6ac763d8a987679f3c2",
+    "url": "/booking_page/static/js/main.50bc4fea.chunk.js"
+  },
+  {
+    "revision": "054477c7ce159088087f",
+    "url": "/booking_page/static/js/runtime-main.688c4cc7.js"
+  },
+  {
+    "revision": "f302982dfeb24965709b1c2fef9e6576",
+    "url": "/booking_page/static/media/img.f302982d.jpg"
+  }
+]);
